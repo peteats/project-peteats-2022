7ddd2059f25026738aaa0c72f4f02f362ec1fd30
Aya <90342033+Aya-X@users.noreply.github.com>
2022-08-22 14:14:15 +0800

    feat: Add useRoutes to App
    Add useRoutes for Dev:
    - pages
    - Layout
    - RoutesConfig

---
+++ src/pages/DevLayout.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+function DevLayout() {
+  return (
+    <>
+      <nav>
+        <ul className="text-blue-700">
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+
+          <li>
+            <Link to="/signup">SIGN-UP</Link>
+          </li>
+
+          <li>
+            <Link to="/login">LOG-IN</Link>
+          </li>
+
+          <li>
+            <Link to="/idontknowwheretogo">404</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr className="h-2 bg-slate-300" />
+
+      <div className="bg-slate-100">
+        <div className="container mx-auto min-h-screen">
+          <section className="px-4 py-8">
+            <Outlet />
+          </section>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DevLayout;
---
+++ src/pages/DevLogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DevLogin() {
+  return (
+    <>
+      <h3>DevLogin</h3>
+      <main>THIS IS DevLogin.</main>
+    </>
+  );
+}
+
+export default DevLogin;
