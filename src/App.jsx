8ce6a4007fff7828996089f7c69de1277619a84b
Aya <90342033+Aya-X@users.noreply.github.com>
2022-09-05 15:44:24 +0800

    feat: Add layout draft of PageHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import React, { useState } from 'react';
+// import { ToastContainer } from 'react-toastify';
 
 import RoutesConfig from './pages/RoutesConfig';
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <RoutesConfig />
       <hr />
+      {/* <ToastContainer /> */}
     </>
   );
 }
