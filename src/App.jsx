4eb6914edbd171ed1447af4471663dcac1c69594
Aya <90342033+Aya-X@users.noreply.github.com>
2022-09-12 15:13:03 +0800

    refactor: Update the main to keep entry simply
    Move to App:
    - Axios Interceptor
    - Toastify Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 // import React, { useState } from 'react';
-// import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
+import AxiosInterceptors from './utils/AxiosInterceptors';
 import RoutesConfig from './pages/RoutesConfig';
 
 function App() {
   return (
-    <>
+    <AxiosInterceptors>
       <RoutesConfig />
-      <hr />
-      {/* <ToastContainer /> */}
-    </>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </AxiosInterceptors>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 // import { BrowserRouter } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 
-import { ToastContainer } from 'react-toastify';
-
-import AxiosInterceptors from './utils/AxiosInterceptors';
-
 import 'react-toastify/dist/ReactToastify.css';
 
 import './index.css';
@@ -17,21 +13,7 @@ ReactDOM.render(
   <React.StrictMode>
     {/* <BrowserRouter> */}
     <HashRouter>
-      <AxiosInterceptors>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-
-        <App />
-      </AxiosInterceptors>
+      <App />
     </HashRouter>
     {/* </BrowserRouter> */}
   </React.StrictMode>,
