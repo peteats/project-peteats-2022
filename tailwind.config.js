ece22797153eed2763269369a6cfd987855b1f9a
Aya <90342033+Aya-X@users.noreply.github.com>
2022-09-26 13:30:41 +0800

    feat: Add image files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'hero-pattern': "url('/img/hero-pattern.svg')",
-        'home-neko': "url('/src/images/bg-neko.png')",
+        'home-neko': "url('/src/images/bg-home-neko.png')",
+        'home-dog': "url('/src/images/bg-home-dog.png')",
+        'home-food': "url('/src/images/bg-home-food.png')",
+        'home-claw': "url('/src/images/bg-home-claw.png')",
         'footer-texture': "url('/img/footer-texture.png')",
       },
     },
