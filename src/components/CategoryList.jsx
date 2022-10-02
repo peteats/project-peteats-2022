93aee992af1fb3b515e5b74eded0c0b66f222688
Aya <90342033+Aya-X@users.noreply.github.com>
2022-10-02 19:53:30 +0800

    feat: Add MOCK_DATA for dev cos Server-ERROR

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -6,42 +6,78 @@ import apiHelper from '../utils/helpers';
 import CategorySkeleton from './CategorySkeleton';
 import CategoryItem from './CategoryItem';
 
+const MOCK_DATA = {
+  Status: true,
+  Data: [
+    {
+      Id: 1,
+      ProductClassName: '生食',
+      imageUrl:
+        'https://peteats.rocket-coding.com/Image/Class/ProductClasses_01.png',
+    },
+    {
+      Id: 2,
+      ProductClassName: '鮮食',
+      imageUrl:
+        'https://peteats.rocket-coding.com/Image/Class/ProductClasses_02.png',
+    },
+    {
+      Id: 3,
+      ProductClassName: '飼料',
+      imageUrl:
+        'https://peteats.rocket-coding.com/Image/Class/ProductClasses_03.png',
+    },
+    {
+      Id: 4,
+      ProductClassName: '罐頭',
+      imageUrl:
+        'https://peteats.rocket-coding.com/Image/Class/ProductClasses_04.png',
+    },
+  ],
+};
+/* end of MOCK_DATA */
+
 function CategoryList({ pageType }) {
   // #REVIEW: context.Provider?
   const [cateData, setCateData] = useState([]);
-  const [isFetch, setIsFetch] = useState(null);
+  // const [isFetch, setIsFetch] = useState(null);
 
   useEffect(() => {
-    if (!cateData.length) {
-      apiHelper.getShopTag().then((res) => {
-        console.log(res);
-
-        if (res?.data?.Status) {
-          console.log('getShopTag:::', res?.data?.Data);
-
-          const { Data } = res.data;
-          if (!isFetch) {
-            setCateData(Data);
-          }
-          /* end of IF(!isFetch) */
-        }
-        /* end of IF(Status) */
-      });
-      /* end of API */
-    }
-    return () => {
-      setTimeout(() => {
-        setIsFetch(true);
-      }, 500);
-      /* end of setTimeout() */
-    };
+    const { Data } = MOCK_DATA;
+    setCateData(Data);
   }, [cateData]);
-  /* end of useEffect() */
 
-  if (!cateData.length || !isFetch) {
-    // if (!cateData.length) {
-    return <CategorySkeleton pageType={pageType} />;
-  }
+  // useEffect(() => {
+  //   if (!cateData.length) {
+  //     apiHelper.getShopTag().then((res) => {
+  //       console.log(res);
+
+  //       if (res?.data?.Status) {
+  //         console.log('getShopTag:::', res?.data?.Data);
+
+  //         const { Data } = res.data;
+  //         if (!isFetch) {
+  //           setCateData(Data);
+  //         }
+  //         /* end of IF(!isFetch) */
+  //       }
+  //       /* end of IF(Status) */
+  //     });
+  //     /* end of API */
+  //   }
+  //   return () => {
+  //     setTimeout(() => {
+  //       setIsFetch(true);
+  //     }, 500);
+  //     /* end of setTimeout() */
+  //   };
+  // }, [cateData]);
+  // /* end of useEffect() */
+
+  // if (!cateData.length || !isFetch) {
+  // if (!cateData.length) {
+  //   return <CategorySkeleton pageType={pageType} />;
+  // }
   /* end of IF(!data) */
 
   return (
