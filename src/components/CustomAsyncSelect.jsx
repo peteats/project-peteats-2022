d6733bb0fd8f3f055e6181020fe57fa251895bba
Aya <90342033+Aya-X@users.noreply.github.com>
2022-10-02 12:33:46 +0800

    feat: Add input search component at PageHome

---
+++ src/components/CustomAsyncSelect.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react';
+
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import CircularProgress from '@mui/material/CircularProgress';
+
+function sleep(delay = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+function CustomAsyncSelect({ data }) {
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  const loading = open && options.length === 0;
+
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      await sleep(1e3); // For demo purposes.
+
+      if (active) {
+        setOptions([...data]);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  return (
+    <Autocomplete
+      id="asynchronous-demo"
+      sx={{ width: 300 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      isOptionEqualToValue={(option, value) => option.title === value.title}
+      getOptionLabel={(option) => option.title}
+      options={options}
+      loading={loading}
+      // JSX-Render
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="立即搜尋商品"
+          // label="Asynchronous"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+}
+
+export default CustomAsyncSelect;
