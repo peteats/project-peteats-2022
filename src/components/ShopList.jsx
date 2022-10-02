d29e647081120f24e0c79b5e0d34c9dbc8008c33
Aya <90342033+Aya-X@users.noreply.github.com>
2022-10-02 19:51:22 +0800

    feat: Add Skeleton to ShopList component for UX-loading

--- src/components/ShopList.jsx
+++ src/components/ShopList.jsx
@@ -1,135 +1,90 @@
 import React, { useEffect, useState } from 'react';
-// import React from 'react';
 import PropTypes from 'prop-types';
 
-import Skeleton from '@mui/material/Skeleton';
-
-import PacmanLoader from 'react-spinners/PacmanLoader';
-
 import apiHelper from '../utils/helpers';
+import ShopsSkeleton from './ShopsSkeleton';
 
 import ShopItem from './ShopItem';
 
+const MOCK_DATA = {
+  Status: true,
+  data: [
+    {
+      Id: 1,
+      ShopName: 'SHOP',
+      EvaluateStars: 4,
+      Views: 5,
+      CityName: 'Taipei',
+      imageUrl:
+        'https://peteats.rocket-coding.com/Image/Class/ProductClasses_01.png',
+    },
+  ],
+};
+/* end of MOCK_DATA */
+
 function ShopList({ queryType, queryId }) {
-  // const [shopsData, setShopsData] = useState([]);
   const [shopsData, setShopsData] = useState(null);
 
   useEffect(() => {
-    if (queryType === 'TAG') {
-      apiHelper.getShopsByTag(queryId).then((res) => {
-        console.log(res);
+    const { data } = MOCK_DATA;
+    setShopsData(data);
+  }, [shopsData]);
 
-        if (res?.data?.Status) {
-          console.log('getShopsByTag:::', res?.data);
+  // useEffect(() => {
+  //   if (queryType === 'TAG') {
+  //     apiHelper.getShopsByTag(queryId).then((res) => {
+  //       console.log(res);
 
-          const { data } = res.data;
-          console.log(data[0]);
-          setShopsData(data);
-        }
-      });
-    }
+  //       if (res?.data?.Status) {
+  //         console.log('getShopsByTag:::', res?.data);
 
-    if (queryType === 'CITY') {
-      apiHelper.getShopsByCity(queryId).then((res) => {
-        console.log(res);
+  //         const { data } = res.data;
+  //         console.log(data[0]);
+  //         setShopsData(data);
+  //       }
+  //     });
+  //   }
 
-        if (res?.data?.Status) {
-          console.log('getShopsByCity:::', res?.data);
+  //   if (queryType === 'CITY') {
+  //     apiHelper.getShopsByCity(queryId).then((res) => {
+  //       console.log(res);
 
-          const { Data } = res.data;
-          console.log(Data[0]);
-          setShopsData(Data);
-        }
-      });
-    }
+  //       if (res?.data?.Status) {
+  //         console.log('getShopsByCity:::', res?.data);
 
-    if (queryType === 'HOT') {
-      apiHelper.getShopsHot().then((res) => {
-        console.log(res);
+  //         const { Data } = res.data;
+  //         console.log(Data[0]);
+  //         setShopsData(Data);
+  //       }
+  //     });
+  //   }
 
-        if (res?.data?.Status) {
-          console.log('getShopsHot:::', res?.data);
+  //   if (queryType === 'HOT') {
+  //     apiHelper.getShopsHot().then((res) => {
+  //       console.log(res);
 
-          const { Message } = res.data;
-          console.log(Message[0]);
-          setShopsData(Message);
-        }
-      });
-    }
-  }, [queryId]);
+  //       if (res?.data?.Status) {
+  //         console.log('getShopsHot:::', res?.data);
 
-  const LoadingItem = (
-    <li className="col-span-12 sm:col-span-4 md:col-span-4 lg:col-span-3">
-      <Skeleton
-        sx={{ height: '360px', borderRadius: '20px' }}
-        // className="rounded-sm"
-        animation="wave"
-        variant="rounded"
-      />
-    </li>
-  );
+  //         const { Message } = res.data;
+  //         console.log(Message[0]);
+  //         setShopsData(Message);
+  //       }
+  //     });
+  //   }
+  // }, [queryId]);
 
   if (!shopsData) {
-    return (
-      <>
-        {/* <h3>LOADING...</h3> */}
-
-        <ul className="grid grid-flow-row auto-rows-auto grid-cols-12 place-content-center gap-4 px-6 sm:px-0">
-          {/* <LoadingItem /> */}
-          {LoadingItem}
-          {LoadingItem}
-          {LoadingItem}
-          {LoadingItem}
-        </ul>
-
-        {/*
-        <section className="mx-auto flex min-h-[25vh] items-center justify-center">
-          <h3>LOADING...</h3>
-
-          <Skeleton
-            className="min-h-[25vh]"
-            animation="wave"
-            variant="rectangular"
-          /> */}
-        {/* <PacmanLoader
-            color="#DB8C8C"
-            loading
-            // cssOverride={override}
-            size={32}
-          /> */}
-        {/* </section> */}
-      </>
-    );
+    return <ShopsSkeleton />;
   }
   /* end of IF(!data) */
 
   return (
-    <>
-      {' '}
-      {/* <div className="pe-container mx-auto my-20 px-12 md:p-0"> */}
-      {/* {queryType === 'HOT' ? null : (
-        <h2 className="my-4 text-left text-2xl font-bold">熱門店家</h2>
-      )} */}
-      {/* <ul className="-ml-8 -mb-8 flex flex-wrap">
-        <StoreCard />
-        <StoreCard />
-        <StoreCard />
-        <StoreCard />
-      </ul> */}
-      <ul className="grid grid-flow-row auto-rows-auto grid-cols-12 place-content-center gap-4 px-6 sm:px-0">
-        {/* <ul className="-ml-6 -mb-8 flex flex-wrap px-6 md:px-0"> */}
-        {shopsData.map((item) => {
-          console.log('!', item);
-          // apiHelper.getImg(item?.imageUrl).then((res) => {
-          //   console.log('res:', res);
-          // });
-
-          return <ShopItem key={item.Id} data={item} />;
-        })}
-      </ul>
-      {' '}
-      {/* </div> */}
-    </>
+    <ul className="grid grid-flow-row auto-rows-auto grid-cols-12 place-content-center gap-4 px-6 sm:px-0">
+      {shopsData.map((item) => (
+        <ShopItem key={item.Id} data={item} />
+      ))}
+    </ul>
   );
 }
 /* end of StoreList() */
